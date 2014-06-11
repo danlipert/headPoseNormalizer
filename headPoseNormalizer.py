fdcfc4e4f36d3a4713411e92bbd01bbfecec332e
Richard Raseley <richard@raseley.com>
2014-06-10 19:02:17 -0700

    created Face and Eye classes and implemented serializer

--- headPoseNormalizer.py
+++ headPoseNormalizer.py
@@ -1,4 +1,5 @@
 import cv2
+import json
 import numpy
 
 imageSource = 'sample_images/one_person.jpg'
@@ -7,12 +8,35 @@ cascadeEyePath = '/usr/share/opencv/haarcascades/haarcascade_eye.xml'
 
 cascadeFacePath = '/usr/share/opencv/haarcascades/haarcascade_frontalface_default.xml'
 
+class Face:
+  """
+  """
+
+  def __init__(self,x,y,w,h,eyes=[]):
+    self.x = int(x)
+    self.y = int(y)
+    self.w = int(w)
+    self.h = int(h)
+    self.eyes = eyes
+
+class Eye:
+  """
+  """
+
+  def __init__(self,x,y,w,h):
+    self.x = int(x)
+    self.y = int(y)
+    self.w = int(w)
+    self.h = int(h)
+
+class Object:
+  def to_JSON(self):
+    return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
+
 def convertGray(imageObject):
   """
   Loads image and converts it to grayscale.
   """
-
-  print 'convertGray'
 
   return cv2.cvtColor(imageObject,cv2.COLOR_BGR2GRAY)
 
@@ -20,36 +44,28 @@ def detectFeatures(imageObjectGray):
   """
   Loads grayscale image, detects faces, and returns their positions.
   """
-  
-  print 'detectFeatures'
 
-  features = {}
-
-  faceCounter = 0
+  features = []
 
   faces = cv2.CascadeClassifier(cascadeFacePath).detectMultiScale(imageObjectGray,1.3,5)
 
   for x,y,w,h in faces:
-    
-    features['face' + str(faceCounter)] = [x,y,w,h]
 
-    print features
+    face = Face(x,y,w,h)
 
-    eyeCounter = 0
+    eyes = cv2.CascadeClassifier(cascadeEyePath).detectMultiScale(imageObjectGray[y:y+h,x:x+w],1.1,3)
 
-    eyes = cv2.CascadeClassifier(cascadeEyePath).detectMultiScale(imageObjectGray[y:y+h,x:x+w],1.3,5)
+    print len(eyes)
 
-    for eye in eyes:
+    for x,y,w,h in eyes:
 
-      features['face' + str(faceCounter)]['eye' + str(eyeCounter)] = {}
-      features['face' + str(faceCounter)]['eye' + str(eyeCounter)] = eye
+      eye = Eye(x,y,w,h)
 
-      eyeCounter += 1
+      face.eyes.append(eye)
 
-    faceCounter += 1
+    features.append(face)
 
   return features
-
 
 def cropFaces(imageObjectGray):
   """
@@ -66,53 +82,15 @@ def normalizeHeadGeometry(faceObjectGray):
   print 'normalizeHeadGeometry'
 
 def main():
-  
+
   imageObjectGray = convertGray(cv2.imread(imageSource))
 
-  features = {detectFeatures(imageObjectGray)}
+  features = detectFeatures(imageObjectGray)
+  
+  test = Object()
 
-  print str(features)
+  test.data = features
 
-  # faceObjectGray = cropFace(imageObjectGray)
-
-  # normalizeHeadGeometry(faceObjectGray)
+  print test.to_JSON()
 
 main()
-
-"""
-cascadeEyesPath = '/usr/share/opencv/haarcascades/haarcascade_eye.xml'
-cascadeFacesPath = '/usr/share/opencv/haarcascades/haarcascade_frontalface_default.xml'
-imagePath = 'sample_images/one_person.jpg'
-
-def detectEyes(imageObjectGrayscale,x,y,w,h):
-  return cv2.CascadeClassifier(cascadeEyesPath).detectMultiScale(imageObjectGrayscale[y:y+h,x:x+w],1.3,5)
-
-def detectFaces(imageObjectGrayscale):
-  return cv2.CascadeClassifier(cascadeFacesPath).detectMultiScale(imageObjectGrayscale,1.3,5)
-
-def main():
-  imageObject = cv2.imread(imagePath)
-  imageObjectGrayscale = cv2.cvtColor(imageObject,cv2.COLOR_BGR2GRAY)
-  
-  results = {}
-  
-  faceCounter = 0 
-  
-  for face in detectFaces(imageObjectGrayscale):
-    results['face' + str(faceCounter)] = {}
-    results['face' + str(faceCounter)]['area'] = face
-    
-    eyeCounter = 0
-    
-    for eye in detectEyes(imageObjectGrayscale,face[0],face[1],face[2],face[3]):
-      results['face' + str(faceCounter)]['eye' + str(eyeCounter)] = {}
-      results['face' + str(faceCounter)]['eye' + str(eyeCounter)]['area'] = eye
-      
-      eyeCounter += 1
-    
-    faceCounter += 1
-
-  print results
-
-main()
-"""
